16a4c64abe2acc4846cc6b09eecbd961bcd78130
FabianeElla <fabianemagalhaes.ella@gmail.com>
2024-08-27 20:35:16 -0300

    Corrigindo input
    Corrigindo lógica para apresentar os dois inputs simultaneamente

--- devops/src/components/App.jsx
+++ devops/src/components/App.jsx
@@ -2,28 +2,36 @@ import React, { useState } from "react";
 
 function App() {
   const [contact, setContact] = useState({
-    fName: ""
+    fName: "",
+    lName: ""
+
   });
 
   function handleChange(event) {
     const { value, name } = event.target;
 
-    setContact(() => {
+    setContact((prevContact) => {
       if (name === "fName") {
         return {
           fName: value,
         };
-      }
+      } else if (name === "lName") {
+        return {
+            fName: prevContact.fName,
+            lName: value,
+        };
+    }
     });
   }
 
   return (
     <div className="container">
       <h1>
-        Hello {contact.fName}
+        Hello {contact.fName} {contact.lName}
       </h1>
       <form>
         <input onChange={handleChange} name="fName" placeholder="First Name" />
+        <input onChange={handleChange} name="lName" placeholder="Last Name" />
         <button>Submit</button>
       </form>
     </div>
